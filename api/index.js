e92fa6e1f4165fdbf8d81c2736b22f68452e653b
njauprince@gmail.com <njauprince@gmail.com>
2023-05-30 14:44:59 +0300

    initial commit

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('dotenv').config()
-const port = process.env.PORT || 5000
+const port = process.env.PORT 
 const colors = require('colors')
 const schema = require('./schema/schema')//graphql schema
 const {graphqlHTTP} = require('express-graphql')
