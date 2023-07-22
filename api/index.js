131a5d32fc1df4362784204dceef8bfedad9c84d
njauprince@gmail.com <njauprince@gmail.com>
2023-07-22 07:19:07 +0300

    Fix dependecies

--- api/index.js
+++ api/index.js
@@ -35,10 +35,10 @@ app.use(express.json());
 
 // module.exports = httpServer;
 app.use(
-  "/api",
+  "/",
   graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === "development",
+    graphiql: true,
   })
 );
 
